bb01225da2488277cc3324a5dff2967f95b32db1
Ania <an.pasieczna@gmail.com>
2022-11-01 15:24:26 +0100

    cancel butto with conditional rendering added

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,8 +1,9 @@
-import React from "react";
+import { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false); //we're setting a new state controlled via click- show or hide the form
   const saveExpenseDataHanlder = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
@@ -10,10 +11,25 @@ const NewExpense = (props) => {
     };
     console.log(expenseData);
     props.onAddExpense(expenseData); //obtained from a parent function, onAddExpense, now as an argument passes expenseData TO the parent.
+    setIsEditing(false);
+  };
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+  const stopEditingHandler = () => {
+    setIsEditing(false);
   };
   return (
     <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHanlder} />
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add new expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHanlder}
+          onCancel={stopEditingHandler}
+        />
+      )}
       {/*prop in the form of a function, hence the name*/}
     </div>
   );
